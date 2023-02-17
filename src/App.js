f18a1b552c4334f4ebab4d43eaf5f36094d2d097
Fin Mead <finleymeadie@hotmail.co.uk>
2023-02-17 22:34:46 +0000

    New font and bg

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AboutPageAnimation from "./AboutPageAnimation";
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="fibonacci"src="https://upload.wikimedia.org/wikipedia/commons/7/79/Fibonacci_spiral.svg"  fill="blue" alt="logo" />
-        <p className='title'>
-          Fin Mead, Portfolio
+        <img className="fibonacci"src="https://upload.wikimedia.org/wikipedia/commons/7/79/Fibonacci_spiral.svg"  alt="logo" />
+        <p className='title' id = 'fancy-text'>
+          FIN MEAD PORTFOLIO
         </p>
         <a
           className="App-link"
@@ -22,8 +23,8 @@ function App() {
             <p className='overview-text'>
             Hi, my name is Fin Mead. I'm a student currently enrolled at UEA in my 3rd year of studying Computer Science. I have many interests and enjoy lots of hobbies. I'm naturally curious and love learning new skills and expanding my knowledge. I have had many projects over the years, big and small. From building devices and programming to film making, 3d design and playing guitar. I enjoy them all, and more!
             </p>
-            <AboutPageAnimation />
         </div>
+        <AboutPageAnimation />
       </header>
     </div>
   );
