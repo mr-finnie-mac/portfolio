88b5a359cea93f8f45663d21359d8c5b6f04135c
Fin Mead <finleymeadie@hotmail.co.uk>
2023-02-18 19:01:57 +0000

    More changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import AboutPageAnimation from "./AboutPageAnimation";
+import './gallery'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="fibonacci"src="https://upload.wikimedia.org/wikipedia/commons/7/79/Fibonacci_spiral.svg"  alt="logo" />
-        <p className='title' id = 'fancy-text'>
-          FIN MEAD PORTFOLIO
-        </p>
+        {/* <img className="fibonacci"src="https://upload.wikimedia.org/wikipedia/commons/7/79/Fibonacci_spiral.svg"  alt="logo" /> */}
+        <div>
+          <h1 className='title' id = 'fancy-text'>
+            FIN MEAD PORTFOLIO
+          </h1>
+        </div>
         <a
           className="App-link"
           href="https://github.com/mr-finnie-mac"
@@ -19,13 +22,33 @@ function App() {
         >
           See github
         </a>
-        <div className="overview">
+        <div>
+          <div id="img-wrapper">
+            <div><img class = 'gall-img' src= {require("./img/gallery/bike-pack.jpg")} alt = "info" /></div>
+            <div><img class = 'gall-img' src= {require("./img/gallery/fruav.jpg")} alt = "info"/></div>
+            <div><img class = 'gall-img' src= {require("./img/gallery/covid-uav.jpg")} alt = "info"/></div>
+            <div><img class = 'gall-img' src= {require("./img/gallery/nor-keyboard.jpg")} alt = "info"/></div>
+            <div><img class = 'gall-img' src= {require("./img/gallery/slider.jpg")} alt = "info"/></div>
+            <div><img class = 'gall-img' src= {require("./img/gallery/esp-remote.jpg")} alt = "info"/></div>
+            <div><img class = 'gall-img' src= {require("./img/gallery/esp-hall-dual-esc.jpg")} alt = "info"/></div>
+            <div><img class = 'gall-img' src= {require("./img/gallery/stereo-testing.jpg")} alt = "info"/></div>
+            <div><img class = 'gall-img' src= {require("./img/gallery/bmx.jpg")} alt = "info"/></div>
+            <div><img class = 'gall-img' src= {require("./img/gallery/wreath-bike.jpg")} alt = "info"/></div>
+            <div><img class = 'gall-img' src= {require("./img/gallery/robot-render.jpg")} alt = "info"/></div>
+            <div><img class = 'gall-img' src= {require("./img/gallery/cgt.jpg")} alt = "info"/></div>
+          </div>
+
+        </div>
+
+        {/* <div className="overview">
             <p className='overview-text'>
             Hi, my name is Fin Mead. I'm a student currently enrolled at UEA in my 3rd year of studying Computer Science. I have many interests and enjoy lots of hobbies. I'm naturally curious and love learning new skills and expanding my knowledge. I have had many projects over the years, big and small. From building devices and programming to film making, 3d design and playing guitar. I enjoy them all, and more!
             </p>
-        </div>
+        </div> */}
         <AboutPageAnimation />
       </header>
+      {/* <div className='title-plaque'>Links to stuff:</div> */}
+      <div className='bottomDweller' id = "fill"></div>
     </div>
   );
 }
---
+++ src/gallery.js
@@ -0,0 +1,14 @@
+// const images = 
+// [
+//     {
+//         src: './img/vint bg.png',
+//         width: 500,
+//         height: 300,
+//     },
+//     {
+//         src: './img/marked bg.png',
+//         width: 500,
+//         height: 300,
+//     },
+
+// ]
