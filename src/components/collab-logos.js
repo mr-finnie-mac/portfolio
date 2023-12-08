7a89430f66c6f59685a727751961b5a21cf48b4f
Fin Mead <finleymeadie@hotmail.co.uk>
2023-12-08 15:50:51 +0000

    added links to noteworthy images and logos

--- src/components/collab-logos.js
+++ src/components/collab-logos.js
@@ -2,13 +2,38 @@ import React from 'react';
 
 function Collaborations() {
   return (
-  <div id="collab-container">
-    <div><img className = 'collab-logo' src= {require("../img/logos/arm-logo.png")} alt = "info"/></div>
-    <div><img className = 'collab-logo' src= {require("../img/logos/hackster_logo.png")} alt = "info"/></div>
-    <div><img className = 'collab-logo' src= {require("../img/logos/Black_lazerthrust.png")} alt = "info"/></div>
-    <div><img className = 'collab-logo' src= {require("../img/logos/NXP-Logo.png")} alt = "info"/></div>
-    <div><img className = 'collab-logo' src= {require("../img/logos/UEA-logo.png")} alt = "info"/></div>
-    <div><img className = 'collab-logo' src= {require("../img/logos/vertical_solar-logo.webp")} alt = "info"/></div>
+    <div id="collab-container">
+      <div>
+        <a href={'https://www.hackster.io/contests/hovergames#category-548'} target="_blank" rel="noopener noreferrer">
+          <img className = 'collab-logo' src= {require("../img/logos/arm-logo.png")} alt = "info"/>
+        </a>
+      </div>
+      <div>
+        <a href={'https://www.hackster.io/Meadeor'} target="_blank" rel="noopener noreferrer">
+          <img className = 'collab-logo' src= {require("../img/logos/hackster_logo.png")} alt = "info"/>
+        </a>
+      </div>
+      <div>
+        <a href={'https://lazerthrust.technology/'} target="_blank" rel="noopener noreferrer">
+        <img className = 'collab-logo' src= {require("../img/logos/Black_lazerthrust.png")} alt = "info"/>
+        </a>
+      </div>
+      <div>
+        <a href={'https://www.hovergames.com/'} target="_blank" rel="noopener noreferrer">
+        <img className = 'collab-logo' src= {require("../img/logos/NXP-Logo.png")} alt = "info"/>
+        </a>
+      </div>
+      <div>
+        <a href={'https://www.uea.ac.uk/'} target="_blank" rel="noopener noreferrer">
+        <img className = 'collab-logo' src= {require("../img/logos/UEA-logo.png")} alt = "info"/>
+        </a>
+      </div>
+
+      <div>
+        <a href={'https://www.verticalsolar.co.uk/'} target="_blank" rel="noopener noreferrer">
+        <img className = 'collab-logo' src= {require("../img/logos/vertical_solar-logo.webp")} alt = "info"/>
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/gallery.js
+++ src/components/gallery.js
@@ -3,17 +3,72 @@ import React from 'react';
 function Gallery() {
   const imageContext = require.context('../img/gallery', false, /\.(jpg|jpeg|png|gif)$/);
   const imagePaths = imageContext.keys();
-  console.log(imagePaths);
-  // const parts = imagePath.split('/');
-  // // Get the filename with extension
-  // const filenameWithExtension = parts[parts.length - 1];
+
+  // Array of objects containing image path and optional web link
+  const imagesWithLinks = [
+    { path: imagePaths[0], link: 'https://www.thingiverse.com/meadeor/designs' },
+    { path: imagePaths[1], link: null }, // No link for this image
+    { path: imagePaths[2], link: null },
+    { path: imagePaths[3], link: null },
+    { path: imagePaths[4], link: null },
+    { path: imagePaths[5], link: 'https://www.hackster.io/Meadeor/crop-mapping-and-survey-drone-743363' },
+    { path: imagePaths[6], link: 'https://www.hackster.io/Meadeor/crop-mapping-and-survey-drone-743363' },
+    { path: imagePaths[7], link: 'https://www.hackster.io/Meadeor/corona-virus-test-vaccination-transport-drone-67fd78' },
+    { path: imagePaths[8], link: 'https://www.hackster.io/Meadeor/digital-travel-lanyard-86cc82' },
+    { path: imagePaths[9], link: null },
+    { path: imagePaths[10], link: null },
+    { path: imagePaths[11], link: null },
+    { path: imagePaths[12], link: null },
+    { path: imagePaths[13], link: null },
+    { path: imagePaths[14], link: null },
+    { path: imagePaths[15], link: null },
+    { path: imagePaths[16], link: 'https://www.hackster.io/Meadeor/fire-rescue-uav-drone-0a29f4' },
+    { path: imagePaths[17], link: 'https://www.hackster.io/Meadeor/fire-rescue-uav-drone-0a29f4' },
+    { path: imagePaths[18], link: null },
+    { path: imagePaths[19], link: null },
+    { path: imagePaths[20], link: null },
+    { path: imagePaths[21], link: null },
+    { path: imagePaths[22], link: null },
+    { path: imagePaths[23], link: 'https://www.linkedin.com/feed/update/urn:li:activity:7077410592151543808/' },
+    { path: imagePaths[24], link: null },
+    { path: imagePaths[25], link: null },
+    { path: imagePaths[26], link: 'https://www.hackster.io/Meadeor/meno-mirror-menopause-symptom-management-system-dc98db' },
+    { path: imagePaths[27], link: null },
+    { path: imagePaths[28], link: null },
+    { path: imagePaths[29], link: 'https://www.hackster.io/Meadeor/crop-mapping-and-survey-drone-743363' },
+    { path: imagePaths[30], link: null },
+    // { path: imagePaths[31], link: null },
+    // { path: imagePaths[32], link: null },
+    // { path: imagePaths[33], link: null },
+    // { path: imagePaths[34], link: null },
+    
+    
+    
+    // Add more objects for additional images
+  ];
 
   return (
     <div id="img-wrapper">
-      {imagePaths.map((imagePath, index) => (
+      {imagesWithLinks.map((imageInfo, index) => (
         <div key={index}>
-
-          <img className="gall-img" src={imageContext(imagePath)} alt={`${((imagePath.split('/'))[imagePath.split('/').length-1].split('.')[0])}`} loading="lazy" />
+          {/* Check if a link is provided */}
+          {imageInfo.link ? (
+            <a href={imageInfo.link} target="_blank" rel="noopener noreferrer">
+              <img
+                className="gall-img"
+                src={imageContext(imageInfo.path)}
+                alt={`${((imageInfo.path.split('/'))[imageInfo.path.split('/').length-1].split('.')[0])}`}
+                loading="lazy"
+              />
+            </a>
+          ) : (
+            <img
+              className="gall-img"
+              src={imageContext(imageInfo.path)}
+              alt={`${((imageInfo.path.split('/'))[imageInfo.path.split('/').length-1].split('.')[0])}`}
+              loading="lazy"
+            />
+          )}
         </div>
       ))}
     </div>
@@ -21,5 +76,6 @@ function Gallery() {
 }
 
 export default Gallery;
+
 
   
